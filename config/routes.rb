71dd9e5681d2a917d9b9d4d26c50643fe858a38b
EC2 Default User <tamam0308@gmail.com>
2022-07-02 05:40:41 +0000

    posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+ 
   devise_for :users
   
   root 'homes#top'
+  
+  resources :posts, only:[:new, :index, :show]
+  # get 'posts/index'
+  # get 'posts/show'
+  # get 'posts/new'
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
