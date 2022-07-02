f90e5f7b4248dff1af000bc676df9d77b4cef478
EC2 Default User <tamam0308@gmail.com>
2022-07-02 06:53:27 +0000

    Commnets

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   
   has_many :favorites, dependent: :destroy
+  has_many :commnets, dependent: :destroy
   
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_02_063016) do
+ActiveRecord::Schema.define(version: 2022_07_02_064336) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
